4ad1a2fc7b17e12f9fabe694326337083f7cdd21
zinovyevaleks <zinovyevaleks@gmail.com>
2018-04-23 01:42:58 +0700

    Переключение окон логина/регистрации

--- scripts/popup.js
+++ scripts/popup.js
@@ -3,6 +3,7 @@ var faqPopup = document.querySelector('.popup--faq'),
   faqCross = faqPopup.querySelector('.cross'),
   loginPopup = document.querySelector('.popup--login'),
   loginLink = document.querySelector('.login-link'),
+  loginLinks = document.querySelector('.login-link1'),
   loginCross = loginPopup.querySelector('.cross'),
   signupPopup = document.querySelector('.popup--signup'),
   signupCross = signupPopup.querySelector('.cross'),
@@ -33,19 +34,15 @@ faqCross.addEventListener('click', function () {
 loginCross.addEventListener('click', function () {
   closeModal(loginPopup);
 });
-
 signupCross.addEventListener('click', function () {
   closeModal(signupPopup);
-})
-
-for(var i = 0; i <=registrationLinks.length; i++) {
-  registrationLinks[i].addEventListener('click',function () {
-    openModal(signupPopup);
-  })
-}
+});
 
 
 
-
-
-
+for (var i = 0; i <= registrationLinks.length; i++) {
+    registrationLinks[i].addEventListener('click', function() {
+        closeModal(loginPopup);
+        openModal(signupPopup);
+    })
+};
